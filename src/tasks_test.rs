783443b1fad774cc042804b74b784be96f404a0b
Tom van der Sommen <self@tomvdsom.nl>
2024-03-09 12:02:11 +0100

    Added more task-type run-related tests.

--- src/tasks_test.rs
+++ src/tasks_test.rs
@@ -1,5 +1,9 @@
 use super::*;
 
+//
+// BasicTask tests:
+//
+
 #[test]
 fn basic_task() {
     let basic = AlchemistBasicTask {
@@ -15,6 +19,36 @@ fn basic_task() {
         },
     );
     assert_eq!(ret, Result::Ok(()));
+}
+
+#[test]
+fn basic_task_env_vars() {
+    let tmpdir = tempfile::tempdir().unwrap();
+    let tmpfile = tmpdir.path().join("output.txt");
+    let basic = AlchemistBasicTask {
+        command: "sh".to_string(),
+        args: Some(vec![
+            "-c".to_string(),
+            format!("echo $ALCHEMIST_TASKS_TEST_VAR > {}", &tmpfile.display()).to_string(),
+        ]),
+        env: Some(HashMap::from([(
+            "ALCHEMIST_TASKS_TEST_VAR".to_string(),
+            "VAR_VALUE".to_string(),
+        )])),
+        hide: None,
+    };
+    let ret = basic.run(
+        "name",
+        &AlchemistConfig {
+            tasks: HashMap::new(),
+        },
+    );
+
+    assert!(ret.is_ok());
+    assert!(tmpfile.exists());
+    let tmpdata = std::fs::read(tmpfile).unwrap();
+    let output = std::str::from_utf8(&tmpdata).unwrap();
+    assert_eq!("VAR_VALUE\n", output);
 }
 
 #[test]
@@ -42,3 +76,92 @@ fn basic_task_nonzero_exit_code() {
         )))
     );
 }
+
+#[test]
+fn basic_task_spawn_failure() {
+    let basic = AlchemistBasicTask {
+        command: "/etc/passwd".to_string(),
+        args: None,
+        env: None,
+        hide: None,
+    };
+    let ret = basic.run(
+        "name",
+        &AlchemistConfig {
+            tasks: HashMap::new(),
+        },
+    );
+    let alchem_err = ret.as_ref().err().unwrap();
+    let kind = match alchem_err {
+        AlchemistError::IOErrorVariant(oh_no::ErrorContext(v, _s)) => Some(v.kind()),
+        _ => None,
+    };
+    assert_eq!(Some(std::io::ErrorKind::PermissionDenied), kind);
+
+    let err_context_str = match alchem_err {
+        AlchemistError::IOErrorVariant(oh_no::ErrorContext(_v, s)) => s.to_owned(),
+        _ => None,
+    };
+    assert_eq!(Some("Starting basic task name with command `/etc/passwd` either not found or insufficient permissions to run.".to_string()), err_context_str);
+}
+
+//
+// ShellTask tests:
+//
+
+#[test]
+fn shell_task() {
+    let shell = AlchemistShellTask {
+        shell_script: "true".to_string(),
+        hide: None,
+    };
+    let ret = shell.run(
+        "name",
+        &AlchemistConfig {
+            tasks: HashMap::new(),
+        },
+    );
+    assert_eq!(ret, Result::Ok(()));
+}
+
+#[test]
+fn shell_task_nonzero_exit_code() {
+    let shell = AlchemistShellTask {
+        shell_script: "false".to_string(),
+        hide: None,
+    };
+    let ret = shell.run(
+        "name",
+        &AlchemistConfig {
+            tasks: HashMap::new(),
+        },
+    );
+    assert_eq!(
+        ret,
+        Result::Err(AlchemistError::AssertionErrorVariant(oh_no::ErrorContext(
+            AssertionError("Shell script 'name' exited with non-zero exit code.".to_string()),
+            None
+        )))
+    );
+}
+
+//
+// SerialTask tests:
+//
+
+#[test]
+fn serial_task_empty() {
+    let serial = AlchemistSerialTasks {
+        serial_tasks: Vec::new(),
+        hide: None,
+    };
+    let ret = serial.run(
+        "name",
+        &AlchemistConfig {
+            tasks: HashMap::new(),
+        },
+    );
+    assert!(ret.is_ok());
+}
+
+// TODO: Add remaining tests for serial tasks & for Parallel Tasks.
