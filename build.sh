312dfda36254426cf6e86ebd96d370484cb65f09
Tom van der Sommen <self@tomvdsom.nl>
2024-09-08 18:12:19 +0200

    Added `--rm` to `podman run` to delete the container when done.

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 # FIXME: this is just a temporary file because its midnight and I will forget
 
-podman run --arch amd64 -v $(pwd):/var/src dev /root/.cargo/bin/cargo build --release
+podman run --arch amd64 -v $(pwd):/var/src --rm dev /root/.cargo/bin/cargo build --release
