c68d1624f6414318426cae20407f130749f5a3de
Tom van der Sommen <self@tomvdsom.nl>
2024-09-29 13:08:43 +0200

    Re-enabled `set -eu` (errexit & unset checks), quoting where possible.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # TODO: re-enable below, and fix wrong-arch-container chosen, because ...?
-#set -eu
+set -eu
 
-# Sets: PROJECT_DIR, IMAGE_NAME, DEV_IMAGE_LOCAL_NAME, ARTIFACTS_DIR
+# Sets: PROJECT_DIR, IMAGE_NAME, REGISTRY_DNS_NAME, ARTIFACTS_DIR
 SCRIPT_DIR="$(realpath $(dirname $0))"
 . "${SCRIPT_DIR}/vars.sh"
 
@@ -16,9 +16,9 @@ echo '=================='
 echo '= AMD / INTEL 64 ='
 echo '=================='
 echo
-podman run --platform=linux/amd64 -v ${PROJECT_DIR}:/var/src --rm "${REGISTRY_DNS_NAME}/${IMAGE_NAME}" cargo build --release
-if test -f ${PROJECT_DIR}/target/release/alchemist; then
-  cp -p ${PROJECT_DIR}/target/release/alchemist ${ARTIFACTS_DIR}/alchemist-linux-x86_64
+podman run --platform=linux/amd64 -v "${PROJECT_DIR}:/var/src" --rm "${REGISTRY_DNS_NAME}/${IMAGE_NAME}" cargo build --release
+if test -f "${PROJECT_DIR}/target/release/alchemist"; then
+  cp -p "${PROJECT_DIR}/target/release/alchemist" "${ARTIFACTS_DIR}/alchemist-linux-x86_64"
 else
   echo "No build artifact found, aborting"
   exit 1
@@ -30,9 +30,9 @@ echo '=================='
 echo '= ARM 64 ='
 echo '=================='
 echo
-podman run --platform=linux/arm64 -v ${PROJECT_DIR}:/var/src --rm "${REGISTRY_DNS_NAME}/${IMAGE_NAME}" cargo build --release
-if test -f ${PROJECT_DIR}/target/release/alchemist; then
-  cp -p ${PROJECT_DIR}/target/release/alchemist ${ARTIFACTS_DIR}/alchemist-linux-arm64
+podman run --platform=linux/arm64 -v "${PROJECT_DIR}:/var/src" --rm "${REGISTRY_DNS_NAME}/${IMAGE_NAME}" cargo build --release
+if test -f "${PROJECT_DIR}/target/release/alchemist"; then
+  cp -p "${PROJECT_DIR}/target/release/alchemist" "${ARTIFACTS_DIR}/alchemist-linux-arm64"
 else
   echo "No build artifact found, aborting"
   exit 1
